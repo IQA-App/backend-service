4102198e750b633bae85252ece78833a2bd22b4f
Yuriy <yuko40118@gmail.com>
2024-09-04 20:06:49 -0500

    API_UserDTO_UserTable.

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,15 @@
+import { IsEmail, MaxLength, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+    @ApiProperty({ example: 'user@example.com' })
+    @IsEmail()
     email: string;
+
+
+    @ApiProperty({ example: 'password123', description: 'Password must be at least 6 characters long' })
+    @MinLength(6, { message: 'Password must be more than 6 symbols !' })
+    @MaxLength(20, { message: 'Password must be at most 20 characters long' })
+    
     password: string;
 }
