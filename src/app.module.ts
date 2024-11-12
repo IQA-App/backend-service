b63b27cf1a173e885e843b4050ef6ab1ed488f84
Yuriy <yuko40118@gmail.com>
2024-11-11 19:12:48 -0600

    API_Fix_Transaction

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,7 +24,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         username: configService.get('DB_USERNAME'),
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
-        synchronize: false,
+        synchronize: true,  //  
         entities: [__dirname + '/**/*.entity{.js, .ts}'],
       }),
       inject: [ConfigService],
